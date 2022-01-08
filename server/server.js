9fdb2495c74432446841d585159e5b7ba1404962
niranjan-sharma-s <niranjansharma1992@gmail.com>
2022-01-08 16:49:49 +0530

    'commit'

--- server/server.js
+++ server/server.js
@@ -12,12 +12,17 @@ const userRoute = require("./routes/userRoute")
 const loginRoute = require("./routes/loginRoute")
 const orderRoute = require('./routes/ordersRoute')
 
-if(process.env.NODE_ENV === `production`){
-    app.use(`/` , express.static(`client/build`))
-    app.get(`*` , (req , res) => {
-        res.sendFile(path.resolve(__dirname , `client/build/index.html, index.html`))
-    })
-}
+// if(process.env.NODE_ENV === `production`){
+//     app.use(`/` , express.static(`client/build`))
+//     app.get(`*` , (req , res) => {
+//         res.sendFile(path.resolve(__dirname , `client/build/index.html, index.html`))
+//     })
+// }
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use('/api/pizzas/', pizzasRoute)
 app.use("/api/users/" , userRoute)
